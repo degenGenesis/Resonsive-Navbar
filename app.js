ece5b019bee34d7ebaef40cc0e90f920a8cb5e61
Trevor Jones <degengenesis@protonmail.com>
2022-09-26 09:16:01 -0700

    Add: element targeting & event listener

--- app.js
+++ app.js
@@ -3,3 +3,13 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', () => {
+  // console.log(links.classList);
+  console.log(links.classList.contains('random'));
+  console.log(links.classList.contains('links'));
+});
+
