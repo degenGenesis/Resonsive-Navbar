947c7a60a6798065e5958a9bcf215385b8645202
Trevor Jones <degengenesis@protonmail.com>
2022-09-26 09:18:29 -0700

    Add: initial show/hide links logic

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', () => {
   // console.log(links.classList);
-  console.log(links.classList.contains('random'));
+  // console.log(links.classList.contains('random'));
   console.log(links.classList.contains('links'));
+  if (links.classList.contains('show-links')) {
+    links.classList.remove('show-links');
+  } else {
+    links.classList.add('show-links');
+  }
 });
 
